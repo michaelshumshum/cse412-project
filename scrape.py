22ea70a501e977367a07cfd8e432b9f6e1d9831e
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-03-18 18:22:55 -0700

    basic scraping functions

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ import requests
 
 dotenv.load_dotenv()
 
-auth_token = (
+spotify_auth_token = (
     "Bearer "
     + json.loads(
         requests.post(
@@ -18,4 +18,31 @@ auth_token = (
     )["access_token"]
 )
 
-# do stuff
+headers = {
+    "Authorization": spotify_auth_token,
+    "Content-Type": "application/json",
+}
+
+
+def get_artist_id(artist_name: str) -> str:
+    response = requests.get(
+        f"https://api.spotify.com/v1/search?q={artist_name}&type=artist",
+        headers=headers,
+    )
+    return json.loads(response.content)["artists"]["items"][0]["id"]
+
+
+def get_albums(artist_id: str) -> list:
+    response = requests.get(
+        f"https://api.spotify.com/v1/artists/{artist_id}/albums",
+        headers=headers,
+    )
+    return json.loads(response.content)["items"]
+
+
+def get_album_tracks(album_id: str) -> list:
+    response = requests.get(
+        f"https://api.spotify.com/v1/albums/{album_id}/tracks",
+        headers=headers,
+    )
+    return json.loads(response.content)["items"]
