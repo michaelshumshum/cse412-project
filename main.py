765682a3a1b2e950efd74f7b07deb337ab06e696
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-04-30 20:27:02 -0700

    better route

--- main.py
+++ main.py
@@ -23,16 +23,41 @@ def search():
     return render_template("search_request.html")
 
 
-@app.route("/search/artist/<int:artist_id>")
+@app.route("/search/songs")
 @db_session
-def search_artist_by_id(artist_id: int):
-    artist = Artist.get(id=artist_id)
-    if not artist:
-        return abort(404)
+def search_artist_by_id():
+    artist_id = request.args.get("artist", type=int)
+    album_id = request.args.get("album", type=int)
+    genre = request.args.get("genre", type=str)
+
+    query = Song.select()
+    title_filters = []
+
+    if artist_id:
+        artist = Artist.get(id=artist_id)
+        if not artist:
+            return abort(404)
+
+        query = query.filter(lambda s: artist in s.artists)
+
+        title_filters.append(f'by artist "{artist.name}"')
+
+    if album_id:
+        album = Album.get(id=album_id)
+        if not album:
+            return abort(404)
+
+        query = query.filter(lambda s: album is s.album)
+
+        title_filters.append(f'from album "{album.name}"')
+
+    if genre:
+        query = query.filter(lambda s: genre == s.genre)
+        title_filters.append(f'with genre "{genre}"')
 
     result = [
         {**song.to_dict(with_collections=True, related_objects=True)}
-        for song in list(Song.select(lambda s: artist in s.artists))
+        for song in list(query)
     ]
 
     # serialize the nested collections
@@ -84,71 +109,7 @@ def search_artist_by_id(artist_id: int):
         }
 
     return render_template(
-        "search_result.html", title=f"Songs by artist '{artist.name}'", songs=result
-    )
-
-
-@app.route("/search/album/<int:album_id>")
-def search_album(album_id: int):
-    album = Album.get(id=album_id)
-    if not album:
-        return abort(404)
-
-    result = [
-        {**song.to_dict(with_collections=True, related_objects=True)}
-        for song in list(Song.select(lambda s: s.album is album))
-    ]
-
-    # serialize the nested collections
-    for song in result:
-        a_l = []
-        for artist in song["artists"]:
-            d = {
-                "id": artist.id,
-                "name": artist.name,
-            }
-            a_l.append(d)
-        song["artists"] = a_l
-
-        p_l = []
-        for producer in song["producers"]:
-            d = {
-                "id": producer.id,
-                "name": producer.name,
-            }
-            p_l.append(d)
-        song["producers"] = p_l
-
-        w_l = []
-        for writer in song["writers"]:
-            d = {
-                "id": writer.id,
-                "name": writer.name,
-            }
-            w_l.append(d)
-        song["writers"] = w_l
-
-        m_l = []
-        for music_video in song["music_videos"]:
-            d = {
-                "id": music_video.id,
-                "name": music_video.name,
-            }
-            m_l.append(d)
-        song["music_videos"] = m_l
-
-        song["album"] = {
-            "id": song["album"].id,
-            "name": song["album"].name,
-        }
-
-        song["record_label"] = {
-            "id": song["record_label"].id,
-            "name": song["record_label"].name,
-        }
-
-    return render_template(
-        "search_result.html", title=f"Songs in album '{album.name}'", songs=result
+        "search_result.html", title=f"Songs {', '.join(title_filters)}", songs=result
     )
 
 
